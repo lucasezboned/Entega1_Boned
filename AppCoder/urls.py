83fe9a54df19e7a905a4ada8bc4c3236d2c24990
lucas boned <lucasez_@hotmail.com>
2022-05-15 19:02:22 -0300

    inicio proyecto con html desde clase 19 aproximadamente

---
+++ AppCoder/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', inicio),
+    
+    path('creaCurso', curso),
+    
+    path('cursos/', cursos),
+    path('profesores/', profesores),
+    path('estudiantes/', estudiantes),
+    path('entregables/', entregables),
+    
+
+]
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# curso27620
+# django27620
+Este repo es un ejemplo para subir el proyecto de django de la 27620
